649946a8c1be864a477f5f8f22932752379958b5
Rafał Małczyński <rafal.malczynski@gmail.com>
2020-02-09 16:53:24 +0100

    Add pushing of new view controller

--- GitProject/GitProject/ViewController.swift
+++ GitProject/GitProject/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let newViewController = UIViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        newViewController.view.backgroundColor = .red
+        navigationController?.pushViewController(newViewController, animated: true)
     }
 
 
